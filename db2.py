e8707d6f7271d329a7a3d74a3a452b4626f51a94
flonne <flonne@4312bd3a-fb40-11de-a20f-79d6ecec5795>
2009-01-13 21:29:37 +0000

    Updated database contents to match unofficial wiki entries.

--- db2.py
+++ db2.py
@@ -6,11 +6,11 @@ db = _mysql.connect("localhost", "aurica", "misha", "hymmnoserver")
 while True:
 	print "First word: "
 	word1 = "'%s'" % raw_input().replace("'", "\\'")
-	print "First school:"
+	print "First dialect:"
 	school1 = int(raw_input())
 	print "Second word: "
 	word2 = "'%s'" % raw_input().replace("'", "\\'")
-	print "Second school:"
+	print "Second dialect:"
 	school2 = int(raw_input())
 	
 	db.query("insert into hymmnos_mapping values (%s, %s, %i, %i)" % (word1, word2, school1, school2))
