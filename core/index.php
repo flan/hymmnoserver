7e5203bf825033fa5c4706511b1ab8ef5c938303
flonne <flonne@4312bd3a-fb40-11de-a20f-79d6ecec5795>
2009-01-14 21:16:48 +0000

    Forgot a quotation mark.

--- core/index.php
+++ core/index.php
@@ -35,7 +35,7 @@
 						&quot;don't sell or limit this work, and please provide due credit&quot;),
 						you may grab all of its materials using Subversion:
 						&quot;svn co
-						<a href="http://hamsterx.homelinux.org/svn/hymmnoserver/>http://hamsterx.homelinux.org/svn/hymmnoserver/</a>
+						<a href="http://hamsterx.homelinux.org/svn/hymmnoserver/">http://hamsterx.homelinux.org/svn/hymmnoserver/</a>
 						hymmnoserver/&quot;.
 						You may also browse the repository using a web browser or any DAV-capable
 						client.
