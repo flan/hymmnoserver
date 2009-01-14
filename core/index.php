b82e3b0ff6ad407fb03d1f68f43f6f36e3bd2e4a
flonne <flonne@4312bd3a-fb40-11de-a20f-79d6ecec5795>
2009-01-14 21:19:49 +0000

    Updated updates.

--- core/index.php
+++ core/index.php
@@ -149,6 +149,8 @@
 						<tr>
 							<td class="infocell">
 								<ul class="basic-inline" style="font-weight: bold; font-size: 7pt; line-height: 3.5mm;">
+									<li>Source released (14/1/2009)</li>
+									<li>Unofficial words (13/1/2009)</li>
 									<li>Grammar evaluator (7/1/2009)</li>
 									<li>Private beta (6/1/2009)</li>
 									<li>Japanese site's content archived (31/12/2008)</li>
