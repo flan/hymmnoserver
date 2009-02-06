95fbca2b36dbf6bdf9e373a82d3cf32459eb04e6
flonne <flonne@4312bd3a-fb40-11de-a20f-79d6ecec5795>
2009-02-06 21:39:23 +0000

    New features new ready for debugging.

--- core/common/transformations.py
+++ core/common/transformations.py
@@ -4,12 +4,8 @@ import random
 import lookup
 
 _BINASPHERE_REGEXP = re.compile("^=>([A-Zx ]+)EXEC[ _]hymme (\d*[1-9])x1/0[ ]?>>((?:[ ]?\d+)+)$")
-"""Binasphere sequence generator.
-To build pool, count the number of syllables in each line, and divide by the greatest common factor.
-rnd = random.Random(647)
-pool = [0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2]
-rnd.shuffle(pool)
-"""
+_PERSISTANT_START_REGEXP = re.compile("^([A-Za-z]+) ([A-Za-z]+) ([A-Za-z]+) 0x vvi.$")
+_PERSISTANT_END_REGEXP = re.compile("^1x AAs ixi.$")
 
 class Error(Exception):
 	"""
@@ -83,6 +79,16 @@ class FormatError(Error):
 		self.description = unicode(description)
 		
 		
+def _readWord(word, db_con):
+	(word, meaning_english, kana, syntax_class, dialect, decorations, syllables) = lookup.readWord(word, db_con)[0]
+	if syntax_class > 0:
+		if dialect % 50 == lookup.DIALECT['New Testament of Pastalie'] or decorations:
+			reason = "'%s'" % (word)
+			if decorations and not dialect % 50 == lookup.DIALECT['New Testament of Pastalie']:
+				reason += " (carried markup)"
+			raise ContentError("Only Central Standard Note and related dialects are Binasphere-supported (offending word: %s)" % (reason))
+	return (word, syntax_class, dialect, syllables)
+	
 def _reconstructBinasphere(tokens, sequence, size):
 	buffers = [[] for i in range(size)]
 	words = tuple([[] for i in range(size)])
@@ -107,7 +113,7 @@ def _reconstructBinasphere(tokens, sequence, size):
 			
 	return words
 	
-def decodeBinasphere(line):
+def decodeBinasphere(line, db_con):
 	match = _BINASPHERE_REGEXP.match(line)
 	if not match:
 		raise FormatError("Non-Binasphere input provided")
@@ -122,40 +128,164 @@ def decodeBinasphere(line):
 		sequence = sequence[0]
 	sequence = [int(i) for i in sequence]
 	
-	return _reconstructBinasphere(tokens, sequence, size)
+	return _divideAndCapitalise(_reconstructBinasphere(tokens, sequence, size), db_con)
 	
-def _divideAndCapitalise(words, db_con):
+def _divideAndCapitaliseLine(words, db_con):
 	lines = []
 	buffer = []
 	unknown = set()
 	
 	for word in words:
-		(word, meaning_english, kana, syntax_class, dialect, decorations, syllables) = lookup.readWord(word.lower(), db_con)[0]
+		(word, syntax_class, dialect, syllables) = _readWord(word.lower())
 		if syntax_class > 0:
-			if dialect % 50 == lookup.DIALECT['New Testament of Pastalie'] or decorations:
-				reason = "'%s'" % (word)
-				if decorations and not dialect % 50 == lookup.DIALECT['New Testament of Pastalie']:
-					reason += " (carried markup)"
-				raise ContentError("Only Central Standard Note and related dialects are Binasphere-supported (offending word: %s)" % (reason))
 			if syntax_class in lookup.SYNTAX_CLASS_REV['ES(I)'] and buffer: #Trailing ES(I)
 				lines.append(' '.join(buffer))
 				buffer = []
-			buffer.append(lookup.decorateWord(word, syntax_class, decorations, None))
+			buffer.append(word)
 		else:
 			word = word.lower()
 			buffer.append(word)
 			unknown.add(word)
-			
 	return (lines + [' '.join(buffer)], unknown)
 	
-def divideAndCapitalise(words, db_con):
+def _divideAndCapitalise(words, db_con):
 	lookup.initialiseEmotionVerbRegexps(db_con)
 	
 	lines = []
 	unknown_set = set()
 	for token_string in words:
-		(line_list, unknown) = _divideAndCapitalise(token_string, db_con)
+		(line_list, unknown) = _divideAndCapitaliseLine(token_string, db_con)
 		lines.append(line_list)
 		unknown_set.update(unknown)
 	return (lines, sorted(unknown))
+	
+def _dissectSyllables(words, db_con):
+	lines = []
+	buffer = []
+	line_syllables = []
+	unknown = set()
+	
+	for word in words:
+		(word, syntax_class, dialect, syllables) = _readWord(word)
+		if syntax_class > 0:
+			if syntax_class in lookup.SYNTAX_CLASS_REV['ES(I)'] and buffer: #Trailing ES(I)
+				lines.append(' '.join(buffer))
+				buffer = []
+			buffer.append(word)
+			line_syllables += [syllable.upper() + 'x' for syllable in syllables[:-1]] + syllables[-1].upper()
+		else:
+			buffer.append(word.lower())
+			line_syllables.append(word.upper())
+			unknown.add(word)
+	return (lines + [' '.join(buffer)], line_syllables, unknown)
+	
+def _multiplexBinasphere(lines, hashable):
+	output_sequence = []
+	
+	syllable_count = [len(syllables) for syllables in lines]
+	greatest_factor = None
+	for i in range(min(syllable_count), 0, -1):
+		if not [None for s_c in syllable_count if s_c % i]:
+			greatest_factor = i
+			
+	pool = []
+	for (i, n) in enumerate([s_c / greatest_factor for s_c in syllable_count]):
+		pool += [i] * n
+	random.Random(hashable).shuffle(pool)
+	
+	while True:
+		for i in pool:
+			output_sequence.append(lines[i].pop(0))
+		if not [None for p in pool if p]:
+			break
+			
+	sequence = None
+	if len(lines) > 10:
+		sequence = ' '.join(pool)
+	else:
+		sequence = ''.join(pool)
+	return "=> %s EXEC hymme %ix1/0>>%s" % (' '.join(output_sequence), len(lines), sequence)
+	
+def encodeBinasphere(lines, db_con):
+	lookup.initialiseEmotionVerbRegexps(db_con)
+	
+	new_lines = []
+	syllable_lines = []
+	unknown_set = set()
+	for l in lines:
+		(lines, syllable_line, unknown) = _dissectSyllables(l.split(), db_con)
+		new_lines.append(lines)
+		syllable_lines.append(syllable_line)
+		unknown_set.update(unknown)
+	return (_multiplexBinasphere(syllable_lines, lines.join('').lower()), new_lines, sorted(unknown))
+	
+def _applyPersistentEmotionSounds(es_i, es_ii, es_iii, words, db_con):
+	lines = []
+	buffer = []
+	line = []
+	unknown = set()
+	
+	for word in words:
+		(word, syntax_class, dialect, syllables) = _readWord(word)
+		if syntax_class > 0:
+			if syntax_class in lookup.SYNTAX_CLASS_REV['ES(I)'] and buffer: #Trailing ES(I)
+				lines.append(' '.join(buffer))
+				buffer = []
+			if not buffer and not lines and not syntax_class in lookup.SYNTAX_CLASS_REV['ES(I)']:
+				buffer = [es_i, es_ii, es_iii]
+			buffer.append(word)
+			line.append(word)
+		else:
+			word = word.lower()
+			buffer.append(word)
+			line.append(word)
+			unknown.add(word)
+	return (' '.join(line), lines + [' '.join(buffer)], unknown)
+	
+def applyPersistentEmotionSounds(lines, db_con):
+	m = _PERSISTANT_START_REGEXP.match(lines[0])
+	if not m or not _PERSISTANT_END_REGEXP.match(lines[-1]):
+		raise FormatError("Persistant syntax not detected")
+		
+	new_lines = []
+	processed = []
+	unknown_set = set()
+	
+	es_i = None
+	es_ii = None
+	es_iii = None
+	words = lookup.readWord(m.group(1).lower(), db_con)
+	for (word, meaning_english, kana, syntax_class, dialect, decorations, syllables) in words:
+		if syntax_class in lookup.SYNTAX_CLASS_REV['ES(I)']:
+			 es_i = word
+			 break
+	if not es_i:
+		es_i = m.group(1).title()
+		unknown.add(es_i)
+		
+	words = lookup.readWord(m.group(2).lower(), db_con)
+	for (word, meaning_english, kana, syntax_class, dialect, decorations, syllables) in words:
+		if syntax_class in lookup.SYNTAX_CLASS_REV['ES(II)']:
+			 es_ii = word
+			 break
+	if not es_ii:
+		es_ii = m.group(2).lower()
+		unknown.add(es_ii)
+		
+	words = lookup.readWord(m.group(3).lower(), db_con)
+	for (word, meaning_english, kana, syntax_class, dialect, decorations, syllables) in words:
+		if syntax_class in lookup.SYNTAX_CLASS_REV['ES(III)']:
+			 es_iii = word
+			 break
+	if not es_iii:
+		es_iii = m.group(3).title()
+		unknown.add(es_iii)
+		
+	for line in lines[1:-1]:
+		(line, processed_lines, unknown) = _applyPersistentEmotionSounds(es_i, es_ii, es_iii, line.split(), db_con)
+		new_lines.append(line)
+		processed.append(processed_lines)
+		unknown_set.update(unknown)
+		
+	return (["%s %s %s 0x vvi." % (es_i, es_ii, es_iii)] + new_lines + line[-1], processed, sorted(unknown_set))
 	
