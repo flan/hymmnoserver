6b0c705447eca8d87adf379848b226b1bae98701
red.hamsterx <red.hamsterx@4312bd3a-fb40-11de-a20f-79d6ecec5795>
2010-01-09 18:53:06 +0000

    Error-handling.

--- database/mysql2sqlite.py
+++ database/mysql2sqlite.py
@@ -51,19 +51,29 @@ sqlite_cur.execute("""
  )
 """)
 
-mysql_cur.execute("SELECT * FROM hymmnos ORDER BY word ASC")
-for (word, meaning_english, meaning_japanese, dialect, kana, romaji, description, syntax_class, syllables) in mysql_cur.fetchall():
-	sqlite_cur.execute(
-	 u"INSERT INTO hymmnos VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-	 (word.decode('utf-8'), dialect, syntax_class, meaning_english.decode('utf-8'), meaning_japanese.decode('utf-8'), kana.decode('utf-8'), romaji.decode('utf-8'), description and description.decode('utf-8'), syllables.decode('utf-8'))
-	)
+try:
+	mysql_cur.execute("SELECT * FROM hymmnos ORDER BY word ASC")
+	for (word, meaning_english, meaning_japanese, dialect, kana, romaji, description, syntax_class, syllables) in mysql_cur.fetchall():
+		sqlite_cur.execute(
+		 u"INSERT INTO hymmnos VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+		 (word.decode('utf-8'), dialect, syntax_class, meaning_english.decode('utf-8'), meaning_japanese.decode('utf-8'), kana.decode('utf-8'), romaji.decode('utf-8'), description and description.decode('utf-8'), syllables.decode('utf-8'))
+		)
+except:
+	print "Error while populating SQLite database (hymmnos)"
+	print "Word responsible: %s:%i" % (word, dialect)
+	exit()
 	
-mysql_cur.execute("SELECT * FROM hymmnos_mapping ORDER BY source ASC, destination ASC")
-for (source, destination, source_dialect, destination_dialect) in mysql_cur.fetchall():
-	sqlite_cur.execute(
-	 u"INSERT INTO hymmnos_mapping VALUES (?, ?, ?, ?)",
-	 (source.decode('utf-8'), destination.decode('utf-8'), source_dialect, destination_dialect)
-	)
+try:
+	mysql_cur.execute("SELECT * FROM hymmnos_mapping ORDER BY source ASC, destination ASC")
+	for (source, destination, source_dialect, destination_dialect) in mysql_cur.fetchall():
+		sqlite_cur.execute(
+		 u"INSERT INTO hymmnos_mapping VALUES (?, ?, ?, ?)",
+		 (source.decode('utf-8'), destination.decode('utf-8'), source_dialect, destination_dialect)
+		)
+except:
+	print "Error while populating SQLite database (hymmnos_mapping)"
+	print "Mapping: %s:%i <-> %s:%i" % (source, source_dialect, destination, destination_dialect)
+	exit()
 	
 mysql_cur.close()
 mysql_db.close()
