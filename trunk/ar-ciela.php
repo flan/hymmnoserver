39745246b765854f6a70c301e0ee26d216f81a17
red.hamsterx <red.hamsterx@4312bd3a-fb40-11de-a20f-79d6ecec5795>
2010-11-21 02:58:04 +0000

    Added ar-ciela (basic).

---
+++ trunk/ar-ciela.php
@@ -0,0 +1,526 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<?php/*
+All code, unless otherwise indicated, is original, and subject to the terms of
+the GNU GPLv3 or, at your option, any later version of the GPL.
+
+All content is derived from public domain, promotional, or otherwise-compatible
+sources and published uniformly under the
+Creative Commons Attribution-Share Alike 3.0 license.
+
+See license.README for details.
+ 
+(C) Neil Tallim, 2009
+*/?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Hymmnoserver - Grammar (Standard)</title>
+		<?php include 'common/resources.xml'; ?>
+		<style type="text/css">
+			table.coding{
+				border-collapse: separate;
+				text-align: center;
+			}
+			table.coding > td{
+				vertical-align: middle;
+			}
+			td.property{
+				font-weight: bold;
+				text-align: left;
+			}
+			td.ciela{
+				font-size: 4.5em;
+				font-family: "Ar-Ciela(Compartment)", sans;
+				text-align: left;
+				padding-left: 4px;
+				color: black;
+			}
+			td.mcl{
+                background-color: #E9E9FF;
+			}
+		</style>
+	</head>
+	<body>
+		<?php include 'common/header.xml'; ?>
+		<div class="text-basic">
+            <h1 class="h04_1">アル・シエラとは</h1>
+            <p>
+                アル・シエラは、通称「星語」と呼ばれる言語であり、人間の言葉ではない。それは惑星が惑星の意志同士で会話をするときや、
+                惑星が何かを紡ぎ出すとき（＝この地球でいうのなら、惑星の活動の全て（気象、噴火、マントル対流、生命サイクルなど）を指す）に、このアル・シエラを用い、
+                自らの想いを具現化するのである。すなわちアル・シエラは惑星アルシエルの惑星活動の源とも言うべき言語なのである。
+            </p>
+            <h1 class="h04_1">アル・シエラ言語の特徴</h1>
+            <p>
+                アルシエラ言語は、可聴範囲内での発音では完全な意味をとることが出来ない。
+                これは、この言語の発音自体が導体H波となり、そのまま世界に作用する事が出来る言語だからである。
+                通常の可聴範囲は２０〜２００００Hzと言われているが、アルシエラはゆうにその３０倍以上、
+                ２０〜６０００００Hzの導体H波で発音する。この帯域を（聴ける聴けないは別として）
+                サンプリング録音する為には、サンプリングレートは1200kHz必要になる（CDは44.1kHz）。
+            </p>
+            <p>
+                では、普段聞こえてくるアルシエラとはどのようなものなのか。
+                我々に置き換えて具体的に体験する方法が１つだけある。それは、我々の声や歌をPCで録音し、
+                波形編集ソフトで100Hz以上をカットオフするのである。
+                すると、恐ろしくこもった声になり、何を言っているのかサッパリわからなくなる。
+                これが、アルシエラを人間が聴いたときの状態そのものである。
+            </p>
+            <p>
+                たとえば、「おかあさん」と喋ったとしよう。
+                これを100Hzでカットオフすると「おほおほん」という感じに聞こえる。
+                ここから聞き取れるのは、5文字であること、アクセントが２，３文字目にあること、母音のニュアンス程度である。
+                実際「おかあさん」以外にも「のたあはう」と言っていたとしても、おなじように「おほおほん」になる。
+                この事から察することが出来ると思うが、例えば「Tisia」という１つの単語で、相当数の意味を持つのである。
+                もちろん、60万Hzまで聞くことが出来れば、その相当数の意味はそれぞれ１つずつ、違う発音の違う単語である事がわかる。
+            </p>
+            <p>
+                日本人的に更にわかりやすい例えで言えば、英語の「草」と「ガラス」である。
+                日本人は英語発音で重要なの高域成分を聞き取る訓練をしていない為、
+                上記2つの発音「グラース」の聞き分けが困難である。これの更に絶望的なまでに聞き分けられないものと言えよう。
+            </p>
+            <div style="text-align: center;">
+                <img src="./static/arciela_fft.jpg" alt="Human-observable frequency range versus Ar ciela frequency range"/>
+                ↑可聴範囲内で同じような波形でも、それより高周波にある音が全く異なれば<br>「聞こえていないから同じ言葉に聞こえる」という事に他ならない。
+            </div>
+            <p>
+                このように、現在の人間にはアルシエラの真の意味を聞き分けることが出来ない為、
+                聞き分けられるレベルでカテゴリ化を行い、更にその中で三次元的（立体マトリックス的）に視覚的細分化を行った。
+                それがアルシエラである。
+            </p>
+            <p>
+                アルシエラを表記するには、以下に説明する2種類の表記方法が存在する。
+            </p>
+            <h1 class="h04_1">【表記法１】アルシエラ・パブリック</h1>
+            <p>
+                パブリック文字は、人間に聞こえる範囲で聞き分けられる音を26文字に落とし込んだものである。
+                通常人間は、この26文字で表現する単語以上の表情を、その単語から読み取ることが出来ない。
+                これはその「開き直った」、聞こえる限りの単語を文字化したものである。
+                CDの解説書に書かれているアルシエラフォントは、このパブリック文字である。
+            </p>
+            <h1 class="h04_1">【表記法２】アルシエラ・コンパートメント</h1>
+            <p>
+                コンパートメント文字は、パブリック文字に注釈を付けることで、人間には聞き取れない領域の周波数帯で
+                どのような波形が存在しているか、しいてはそれが聞き取れたときどんな文字になるか、を
+                マトリックス的に表現するものである。<br>
+                通常、アルシエラでの会話を聞いて、それをコンパートメント文字で書くことは人間には出来ない。
+                また、人間がアルシエラを書くとき、コンパートメント文字を使ってしっかりとした一対一の意味を作れるほど
+                アルシエラに精通していない。
+                だが、より科学的にアルシエラを解析する為にはその基準を作る為の文字が必要である。
+            </p>
+            <p>
+                コンパートメントは、パブリック文字1文字を更に、周波数傾向５種類と振幅傾向４種類に分離する為に使われる。
+                これをコンパートメントコードと言い、パブリック文字に対しての部位的な扱いで表現される。<br/>
+            （漢字で言えば、鰯、溺、といった魚へんやさんずいに当たる部分がコンパートメントコード）
+            </p>
+            <b>▼ FMCL</b><br/>
+			<table class="coding">
+				<tr>
+					<td class="property mcl">Modifier</td>
+					
+					<td class="mcl"/>
+					<td class="ciela mcl">!</td>
+					<td class="ciela mcl">#</td>
+					<td class="ciela mcl">$</td>
+					<td class="ciela mcl">%</td>
+				</tr>
+				<tr>
+					<td class="property mcl">Category</td>
+					
+					<td class="mcl">session-0</td>
+					<td class="mcl">session-1</td>
+					<td class="mcl">session-2</td>
+					<td class="mcl">session-3</td>
+					<td class="mcl">session-4</td>
+				</tr>
+				<tr>
+					<td class="property mcl">Frequency range</td>
+					
+					<td class="mcl">20000Hz</td>
+					<td class="mcl">50000Hz</td>
+					<td class="mcl">100000Hz</td>
+					<td class="mcl">150000Hz</td>
+					<td class="mcl">300000Hz</td>
+				</tr>
+			</table><br/>
+            <b>▼ AMCL</b><br/>
+			<table class="coding">
+				<tr>
+					<td class="property mcl">Modifier</td>
+					
+					<td class="ciela mcl">&amp;</td>
+					<td class="ciela mcl">(</td>
+					<td class="mcl"/>
+					<td class="ciela mcl">)</td>
+				</tr>
+				<tr>
+					<td class="property mcl">Category</td>
+					
+					<td class="mcl">quad</td>
+					<td class="mcl">dual</td>
+					<td class="mcl">single</td>
+					<td class="mcl">half</td>
+				</tr>
+				<tr>
+					<td class="property mcl">Amplitude signature</td>
+					
+					<td class="mcl">＼＿＿＿</td>
+					<td class="mcl">／￣＼＿</td>
+					<td class="mcl">＿＿＿＿</td>
+					<td class="mcl">/\&nbsp;&nbsp;&nbsp;</td>
+				</tr>
+			</table><br/>
+            <p>
+                コンパートメントコードは、２００００Hz〜６０００００Hzまでの間を対数的に等分し、それぞれに対して
+                コンパートメントレベルという形で表現する。
+                コンパートメントコードにはFMCLとAMCLという2種類が存在する。
+                FMCL（周波数帯域傾向）とは、２００００Hz以上の周波数帯において対数的に5分割した時、
+                その最も音圧が高い区画について、記号化したものになる。
+                例えば、１００Hzで聞いたときのくぐもった「あ」は「か」かもしれないし「さ」かもしれないが、
+                「か」よりも「さ」の方が高周波成分を多く含んでいる。それをザックリと定義している。
+                AMCL（振幅域傾向）とは、その文字を発音している際に、２００００Hz以上の発音音圧の変遷（エンベロープ）が
+                どのように変化するかによって決められている。例えば「か」と「さ」では、前者の方がより鋭利で短時間集中型であり、
+                後者の方がよりソフトにまんべんなく音圧が存在する。<br/>
+            </p>
+            <p>
+                通常、FMCLはパブリック文字の上側に、AMCLは下側に記述する。<br/>
+                尚、コンパートメントコードは母音には使わない。<br/>
+                子音に多く存在する高周波成分の指標である為、母音に使うのは無意味な為である。<br/>
+            </p>
+            <b>▼　実際の導入例</b><br/>
+            <table>
+                <tr>
+    	            <td class="ciela mcl">
+                        Ec Tisia
+    	            </td>
+    	            <td class="ciela mcl">
+                        E%)c T$&amp;is%(ia
+    	            </td>
+	            </tr>
+	            <tr>
+	                <td class="mcl">
+	                    『Ec Tisia』のパブリック表記<br/>
+	                    Ec Tisia
+                    </td>
+                    <td class="mcl">
+                        『Ec Tisia』のコンパートメント表記<br/>
+                        Ec[s-4/harf] T[s-3/quad]is[s-4/dual]ia
+                    </td>
+                </tr>
+            </table>
+            <p>
+                「Ec Tisia」は「共感＋成長」と「皆＋誠心＋赦し＋希望＋想いの力」という意味による綴りである為、コンパートメント表記では『Ec[s-4/harf] T[s-3/quad]is[s-4/dual]ia』となる。
+                それをコンパートメント文字で記すと、上記のような文字になるのである。
+            </p>
+            <h1 class="h04_1">発音の想いについて</h1>
+            <p>
+                アルシエラは律詩前月読の原型的存在であるが為に、律詩前月読と同様に、発音自体にある程度心的作用（＝想いの力＝導体H波）が入る。
+                それによって生成される単語の意味は、その単語ごとに意訳されてわかりやすくなるが、その単語の想いは発音ごとの集合体によって紡がれる。
+                例えば「Ec Tisia」は「共感＋成長」と「皆＋誠心＋赦し＋希望＋想いの力」といった導体H波的な想いを載せて、「全てを赦そう」という意味を形成している。
+                ただし、単語の意味＝文字の想いを繋げたときの意味、とは限らない。
+                あくまでその単語を発音している時の想いであり、ヒュムノス語で言うところの想音を発音全てに含んでいると言った方が正しいかも知れない。
+            </p>
+            <p>
+                下記の「発音想い表」はこれを一元化したものである。
+                母音に波動レベルによる分類がないのは、母音は子音の想いに対してエネルギーを与える役割だからである。
+                波動科学的には、上記のように、コンパートメントに対応できない為に分類分けをしていない。
+                意味がバラバラなようにも感じるかもしれないが、惑星意志にとってこれらの意味は全部同義である。
+                それを敢えて一言で言えば【】表記のような単語になる。ただしこれも、人間が勝手に定義したものだ。
+                それ以上の定義（例えば星語（アルシエラ）で一言に置き換えると…など）は出来ない。
+                なぜなら、アルシエラで作られた単語はそれ自体が想いのポリマー的生成物であり、
+                例えば「A」を説明する単語の中に「U」やら「N」やらが入るわけで
+                その時点でその単語は既に不純であり的を得ていない。
+                母音や子音の想いを言葉で表現するのは不可能なのである。
+            </p>
+            <table>
+                <tr>
+                    <td/>
+                    <td colspan="9"><b>周波数帯域レベル（レベル毎の代表的な発音に対する想い）</b></td>
+                </tr>
+                <tr>
+                    <td/>
+                    <td>I</td>
+                    <td>II</td>
+                    <td>III</td>
+                    <td>IV</td>
+                    <td>V</td>
+                    <td>VI</td>
+                    <td>VII</td>
+                    <td>VIII</td>
+                    <td>IX</td>
+                </tr>
+                <tr>
+                    <td>A</td>
+                    <td colspan="9">【スカラー】中立、平等、公平、エネルギー、想いの力、バランス、愛</td>
+                </tr>
+                <tr>
+                    <td>I</td>
+                    <td colspan="9">【プレイ】畏オソれ、不安フアン、敬ウヤマい、希望キボウ、精神セイシン、慈愛</td>
+                </tr>
+                <tr>
+                    <td>U</td>
+                    <td colspan="9">【イクスピアリ】憧アコガれ、羨望センボウ、好ヨシミ敵テキ、協力キョウリョク、友愛</td>
+                </tr>
+                <tr>
+                    <td>E</td>
+                    <td colspan="9">【ネイバー】憐アワれみ、遠慮エンリョ、理解リカイ、共感キョウカン、性愛</td>
+                </tr>
+                <tr>
+                    <td>O</td>
+                    <td colspan="9">【カルマ】憎悪ゾウオ、怒イカり、悲カナしみ、諭サトし、自己愛</td>
+                </tr>
+                <tr>
+                    <td>N</td>
+                    <td colspan="9">【マンダラ】無ム、楽ラク、悟サト、博愛</td>
+                </tr>
+                <tr height=18 style='height:13.5pt'>
+                    <td>b</td>
+                    <td>自身</td>
+                    <td/>
+                    <td>隣人</td>
+                    <td/>
+                    <td>地域</td>
+                    <td/>
+                    <td>世界</td>
+                    <td/>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>c</td>
+                    <td/>
+                    <td>変身</td>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td>革命</td>
+                    <td/>
+                    <td>成長</td>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>d</td>
+                    <td>魔</td>
+                    <td/>
+                    <td>堕落</td>
+                    <td/>
+                    <td>闇</td>
+                    <td/>
+                    <td/>
+                    <td>光</td>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>f</td>
+                    <td>伝導</td>
+                    <td/>
+                    <td/>
+                    <td>波及</td>
+                    <td/>
+                    <td>浸透</td>
+                    <td/>
+                    <td/>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>g</td>
+                    <td>地獄</td>
+                    <td/>
+                    <td>破壊</td>
+                    <td/>
+                    <td>変化</td>
+                    <td/>
+                    <td>再生</td>
+                    <td>育成</td>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>h</td>
+                    <td>熱</td>
+                    <td/>
+                    <td>炎</td>
+                    <td/>
+                    <td/>
+                    <td>燃焼</td>
+                    <td/>
+                    <td>情熱</td>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>j</td>
+                    <td>無知</td>
+                    <td>未知</td>
+                    <td/>
+                    <td>奇抜</td>
+                    <td/>
+                    <td/>
+                    <td>特異</td>
+                    <td/>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>k</td>
+                    <td>K</td>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>l</td>
+                    <td>他人</td>
+                    <td/>
+                    <td>隣人</td>
+                    <td/>
+                    <td>友人</td>
+                    <td/>
+                    <td>伴侶</td>
+                    <td>託生</td>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>m</td>
+                    <td>平静</td>
+                    <td/>
+                    <td/>
+                    <td>同情</td>
+                    <td/>
+                    <td>救</td>
+                    <td/>
+                    <td/>
+                    <td>愛</td>
+                </tr>
+                <tr height=18 style='height:13.5pt'>
+                    <td>p</td>
+                    <td>P</td>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>q</td>
+                    <td/>
+                    <td>愚</td>
+                    <td/>
+                    <td/>
+                    <td>無知</td>
+                    <td/>
+                    <td>無心</td>
+                    <td/>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>r</td>
+                    <td>胎動</td>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td>躍動</td>
+                    <td/>
+                    <td>生命</td>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>s</td>
+                    <td>欲望</td>
+                    <td>すがり</td>
+                    <td/>
+                    <td>願</td>
+                    <td>祈り</td>
+                    <td/>
+                    <td>感謝</td>
+                    <td>赦し</td>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>t</td>
+                    <td/>
+                    <td/>
+                    <td>己</td>
+                    <td>我々</td>
+                    <td/>
+                    <td>皆</td>
+                    <td/>
+                    <td>世界</td>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>v</td>
+                    <td/>
+                    <td>喜び</td>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td>賞賛</td>
+                    <td/>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>w</td>
+                    <td/>
+                    <td/>
+                    <td>内面</td>
+                    <td/>
+                    <td>精神</td>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>x</td>
+                    <td>攻勢</td>
+                    <td>防御</td>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td>守護</td>
+                    <td/>
+                    <td/>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>y</td>
+                    <td>闇</td>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td>聖</td>
+                    <td/>
+                    <td>愛</td>
+                </tr>
+                <tr>
+                    <td>z</td>
+                    <td/>
+                    <td>神</td>
+                    <td>主</td>
+                    <td/>
+                    <td/>
+                    <td>内面</td>
+                    <td/>
+                    <td>己</td>
+                    <td>愛</td>
+                </tr>
+            </table>
+            <p>
+                ※アルキア研究所所蔵資料。研究当時において、何となく解った部分のみが記されている<br/>
+                Ⅰ〜Ⅸの区分は研究所がつけたもので、実際のアルシエラとしての意味は全くない<br/>
+                寧ろ、この1区画（例えばⅢ）を更に10分割しても、新たな「発音の想い」があふれ出てくるであろう<br/>
+            </p>
+		</div>
+		<?php include 'common/footer.xml'; ?> 
+	</body>
+</html>
