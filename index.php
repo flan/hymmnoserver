66f6c6c423aec1939431e9aa244b63f989f0779b
Neil Tallim <flan@solydxk>
2016-01-09 16:40:57 -0700

    Updated the mini-changelog

--- index.php
+++ index.php
@@ -173,11 +173,11 @@ See license.README for details.
 							<tr>
 								<td class="infocell">
 									<ul class="basic-inline" style="font-weight: bold; font-size: 7pt; line-height: 3.5mm;">
-									    <li>(13/12/2010) Ar ciela (basics)</li>
-									    <li>(20/10/2010) Added Ar ciela font</li>
-									    <li>(20/10/2010) Housekeeping</li>
+										<li>(9/1/2016) Added new words</li>
+										<li>(13/12/2010) Ar ciela (basics)</li>
+										<li>(20/10/2010) Added Ar ciela font</li>
+										<li>(20/10/2010) Housekeeping</li>
 										<li>(21/2/2010) At3 words added</li>
-										<li>(10/1/2010) Added new words</li>
 									</ul>
 								</td>
 							</tr>
